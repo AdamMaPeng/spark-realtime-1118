413771a584234c38af452aa87d251760391af6ac
AdamMa <Adam_Ma520@outlook.com>
2022-04-19 21:54:29 +0800

    Spark 实时项目     util.MyConfig: 配置项匹配的类

---
+++ sparkStreaming-realtime/src/main/scala/com/atguigu/gmall/realtime/util/MyConfig.scala
@@ -0,0 +1,15 @@
+package com.atguigu.gmall.realtime.util
+
+/**
+ *  @author Adam-Ma 
+ *  @date 2022/4/19 0:53
+ *  @Project spark-realtime-1118
+ *  @email Adam_Ma520@outlook.com
+ *  @phone 18852895353
+ */
+/**
+*   将 配置 文件中的所有 k,v 存储在对象中，直接在用到的地方调用
+ */
+object MyConfig {
+  val KAFKA_BROKER_LIST = "kafka.broker.list"
+}
